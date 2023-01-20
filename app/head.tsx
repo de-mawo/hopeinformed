ccb871c4d5ff76aa59db8b01fc199af7f5bd11b9
groovebass <groovebassist1@gmail.com>
2023-01-20 17:58:31 +0200

    testing git push using different PC

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title></title>
+      <title>Hope Informed Foundation</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/img/hi-teal.png" />
     </>
   )
 }
